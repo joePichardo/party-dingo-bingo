ce76588ddb2bc0ba975b0d82064077be684d70fa
joepichardo <joepichar@gmail.com>
2020-03-31 22:26:58 -0700

    add email, first and last name

--- backend/bin/sql/account.sql
+++ backend/bin/sql/account.sql
@@ -3,5 +3,8 @@ CREATE TABLE account(
     "usernameHash" CHARACTER(64),
     "passwordHash" CHARACTER(64),
     "sessionId"    CHARACTER(36),
+    email          CHARACTER(70),
+    "firstName"    CHARACTER(50),
+    "lastName"     CHARACTER(50),
     balance        INTEGER NOT NULL
 );

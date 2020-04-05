c88dc5200b5233c60ecf446550f5f02584496ed8
joepichardo <joepichar@gmail.com>
2020-04-04 22:44:38 -0700

    add potValue column

--- backend/bin/sql/game.sql
+++ backend/bin/sql/game.sql
@@ -4,6 +4,7 @@ CREATE TABLE game(
     nickname        VARCHAR(64),
     "isPublic"      BOOLEAN NOT NULL,
     "buyValue"      INTEGER NOT NULL,
+    "potValue"      INTEGER NOT NULL,
     "ownerId"       INTEGER,
     FOREIGN KEY ("ownerId") REFERENCES account(id)
 );

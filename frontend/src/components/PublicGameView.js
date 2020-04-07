9d7cd5f1c2328a713b84b950d16f9ad96bd58de0
joepichardo <joepichar@gmail.com>
2020-04-06 23:53:34 -0700

    add public game view component

---
+++ frontend/src/components/PublicGameView.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PublicGameMemberRow from "./PublicGameMemberRow";
+import {BACKEND} from "../config";
+
+class PublicGameView extends Component {
+
+  state = { gameMembers: [] };
+
+  componentDidMount() {
+    fetch(`${BACKEND.ADDRESS}/game/${this.props.match.params.id}/members`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => response.json())
+      .then(({ gameMembers }) => {
+        this.setState({ gameMembers });
+        console.log("gameMembers", this.state.gameMembers)
+      })
+      .catch(error => alert(error.message));
+  }
+
+  render() {
+    return(
+      <div>
+        <Link to="/">Home</Link>
+        {
+          this.state.gameMembers.map(member => {
+            return (
+              <div key={member.id}>
+                <div>{member.username}</div>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default connect(
+  ({  }) => ({  }),
+  {  }
+)(PublicGameView);

ed1bb3a53d839d9f40658a9d59cb384cc2d3be74
joepichardo <joepichar@gmail.com>
2020-04-25 01:08:24 -0700

    add backend update functionality to updateGameValue, fix elements in component to include error message

--- frontend/src/components/AccountGameValueRow.js
+++ frontend/src/components/AccountGameValueRow.js
@@ -5,6 +5,7 @@ class AccountGameValueRow extends Component {
 
   state = {
     inputValue: this.props.gameValue.textValue,
+    initialValue: this.props.gameValue.textValue
   };
 
   updateInputValue = event => {
@@ -12,23 +13,38 @@ class AccountGameValueRow extends Component {
   };
 
   updateGameValue = () => {
-    console.log("update");
-    // const { dragonId, saleValue } = this.props.dragon;
-    //
-    // fetch(`${BACKEND.ADDRESS}/dragon/buy`, {
-    //   method: 'POST',
-    //   credentials: 'include',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({ dragonId, saleValue })
-    // }).then(response => response.json())
-    //   .then(json => {
-    //     alert(json.message);
-    //
-    //     if (json.type !== 'error') {
-    //       history.push('/account-dragons');
-    //     }
-    //   })
-    //   .catch(error => alert(error.message));
+    if (this.state.inputValue === this.state.initialValue) {
+      this.setState({ modalErrorMessage: "Value was not updated" });
+      return;
+    }
+
+    this.setState({ modalErrorMessage: "" });
+
+    const { gameId, itemId } = this.props.gameValue;
+
+    const gameValue = {
+      gameId,
+      itemId,
+      textValue: this.state.inputValue
+    };
+
+    fetch(`${BACKEND.ADDRESS}/game/${gameId}/values/update`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      body: JSON.stringify(gameValue)
+    }).then(response => response.json())
+      .then(json => {
+        if (json.type === 'error') {
+          this.setState({ modalErrorMessage: json.message })
+        } else {
+          this.setState({ inputValue: json.gameValue.textValue });
+          this.setState({ initialValue: json.gameValue.textValue });
+        }
+      })
+      .catch(error => {
+        this.setState({ modalErrorMessage: error.message });
+      } );
   };
 
   deleteGameValue = () => {
@@ -53,15 +69,22 @@ class AccountGameValueRow extends Component {
 
   render() {
     return (
-      <div className="input-group mb-3" key={this.props.gameValue.itemId}>
-        <input type="text" className="form-control" value={this.state.inputValue} onChange={this.updateInputValue} />
-        <div className="input-group-append">
-          <button className="btn btn-info" type="button" onClick={this.updateGameValue}>
-            Update
-          </button>
-          <button className="btn btn-danger" type="button" onClick={this.deleteGameValue}>
-            Delete
-          </button>
+      <div key={this.props.gameValue.itemId}>
+        <div className="text-danger">
+          {
+            this.state.modalErrorMessage ? <p className="mb-2">{this.state.modalErrorMessage}</p> : <p className="mb-0"></p>
+          }
+        </div>
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" value={this.state.inputValue} onChange={this.updateInputValue} />
+          <div className="input-group-append">
+            <button className="btn btn-info" type="button" onClick={this.updateGameValue}>
+              Update
+            </button>
+            <button className="btn btn-danger" type="button" onClick={this.deleteGameValue}>
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     )

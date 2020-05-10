f4b74ca53403c554a625b5779b913dde28780b73
joepichardo <joepichar@gmail.com>
2020-05-10 07:49:06 -0700

    add tabs to show game values and game members

--- frontend/src/components/PublicGameView.js
+++ frontend/src/components/PublicGameView.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PublicGameMemberRow from "./PublicGameMemberRow";
 import {BACKEND} from "../config";
-import {Button} from "react-bootstrap";
+import {Button, Tab, Tabs} from "react-bootstrap";
 
 class PublicGameView extends Component {
 
-  state = { gameMembers: [] };
+  state = {
+    gameMembers: [],
+    gameValues: []
+  };
 
   constructor(props){
     super(props);
@@ -27,6 +30,17 @@ class PublicGameView extends Component {
         this.setState({ gameMembers });
       })
       .catch(error => alert(error.message));
+
+    fetch(`${BACKEND.ADDRESS}/game/${this.props.match.params.id}/values`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => response.json())
+      .then(({ gameValues }) => {
+        if (gameValues !== undefined) {
+          this.setState({ gameValues: gameValues });
+        }
+      })
+      .catch(error => alert(error.message));
   }
 
   render() {
@@ -35,15 +49,37 @@ class PublicGameView extends Component {
         <div className="row">
           <div className="col-12">
             <Button onClick={this.goBack}>Go Back</Button>
-            {
-              this.state.gameMembers.map(member => {
-                return (
-                  <div key={member.id}>
-                    <PublicGameMemberRow member={member} />
-                  </div>
-                )
-              })
-            }
+
+            <Tabs className="mt-5" defaultActiveKey="values" transition={false} id="noanim-tab-example">
+              <Tab eventKey="values" title="Values">
+                <ul className="list-group">
+                  {
+                    this.state.gameValues.map(value => {
+                      value.gameId = this.props.match.params.id;
+                      return (
+                        <li key={value.itemId} className="list-group-item">
+                          {value.textValue}
+                        </li>
+                      );
+                    })
+                  }
+                </ul>
+              </Tab>
+              <Tab eventKey="players" title="Players">
+                <ul className="list-group">
+                  {
+                    this.state.gameMembers.map(member => {
+                      return (
+                        <li className="list-group-item" key={member.id}>
+                          <PublicGameMemberRow member={member} />
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </Tab>
+            </Tabs>
+
           </div>
         </div>
       </div>

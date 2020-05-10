d12ee02dda38f0b20561750919b23978c6df46b0
joepichardo <joepichar@gmail.com>
2020-05-10 09:54:29 -0700

    update table to delete on cascade if game or value is deleted

--- backend/bin/sql/gameMemberData.sql
+++ backend/bin/sql/gameMemberData.sql
@@ -1,8 +1,8 @@
 CREATE TABLE gameMemberData(
-    "gameId"      INTEGER REFERENCES game(id),
+    "gameId"      INTEGER NOT NULL,
     "itemId"     INTEGER NOT NULL,
-    "accountId"   INTEGER REFERENCES account(id),
+    "accountId"   INTEGER NOT NULL,
     "positionId"  INTEGER NOT NULL,
     PRIMARY KEY ("gameId", "itemId", "accountId"),
-    FOREIGN KEY ("gameId", "itemId") REFERENCES gameValue("gameId", "itemId")
+    FOREIGN KEY ("gameId", "itemId") REFERENCES gameValue("gameId", "itemId") ON DELETE CASCADE
 );

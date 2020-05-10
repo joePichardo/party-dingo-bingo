2d06124ee638a58c1dcc2bc1ca068a0f77b1aea1
joepichardo <joepichar@gmail.com>
2020-05-09 23:52:33 -0700

    add admission end date and game end date

--- backend/bin/sql/game.sql
+++ backend/bin/sql/game.sql
@@ -1,6 +1,8 @@
 CREATE TABLE game(
     id              SERIAL PRIMARY KEY,
     birthdate       TIMESTAMP NOT NULL,
+    "admissionEndDate"     TIMESTAMP NOT NULL,
+    "gameEndDate"       TIMESTAMP NOT NULL,
     nickname        VARCHAR(64),
     "isPublic"      BOOLEAN NOT NULL,
     "buyValue"      INTEGER NOT NULL,

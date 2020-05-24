eb854c1054672d1774fbc331c0dd23e391cce24f
joepichardo <joepichar@gmail.com>
2020-05-24 09:25:17 -0700

    rating boolean column added

--- backend/bin/sql/gameValue.sql
+++ backend/bin/sql/gameValue.sql
@@ -2,6 +2,7 @@ CREATE TABLE gameValue(
     "gameId"    INTEGER NOT NULL,
     "itemId"   INTEGER NOT NULL,
     "textValue" VARCHAR(140),
+    rating BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY ("gameId", "itemId"),
     FOREIGN KEY ("gameId") REFERENCES game(id) ON DELETE CASCADE
 );

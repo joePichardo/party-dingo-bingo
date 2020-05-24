8e64740065c1b88a3d83dd242112e2eda0881a74
joepichardo <joepichar@gmail.com>
2020-05-24 09:27:29 -0700

    add function, state and elements to update rating value

--- frontend/src/components/AccountGameValueRow.js
+++ frontend/src/components/AccountGameValueRow.js
@@ -5,7 +5,8 @@ class AccountGameValueRow extends Component {
 
   state = {
     inputValue: this.props.gameValue.textValue,
-    initialValue: this.props.gameValue.textValue
+    initialValue: this.props.gameValue.textValue,
+    ratingValue: this.props.gameValue.rating
   };
 
   updateInputValue = event => {
@@ -47,6 +48,37 @@ class AccountGameValueRow extends Component {
       } );
   };
 
+  updateRatingValue = () => {
+
+    this.setState({ modalErrorMessage: "" });
+
+    const { gameId, itemId } = this.props.gameValue;
+
+    const gameValue = {
+      gameId,
+      itemId,
+      rating: !this.state.ratingValue
+    };
+
+    fetch(`${BACKEND.ADDRESS}/game/${gameId}/values/update`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      body: JSON.stringify(gameValue)
+    }).then(response => response.json())
+      .then(json => {
+        if (json.type === 'error') {
+          this.setState({ modalErrorMessage: json.message })
+        } else {
+          this.setState({ ratingValue: gameValue.rating });
+          console.log('json', json);
+        }
+      })
+      .catch(error => {
+        this.setState({ modalErrorMessage: error.message });
+      } );
+  };
+
   render() {
     return (
       <div key={this.props.gameValue.itemId}>
@@ -57,14 +89,22 @@ class AccountGameValueRow extends Component {
         </div>
         <div className="input-group mb-3">
           <input type="text" className="form-control" value={this.state.inputValue} onChange={this.updateInputValue} />
-          <div className="input-group-append">
-            <button className="btn btn-info" type="button" onClick={this.updateGameValue}>
-              Update
-            </button>
-            <button className="btn btn-danger" type="button" onClick={() => this.props.deleteGameValue(this.props.gameValue.gameId, this.props.gameValue.itemId)}>
-              Delete
-            </button>
-          </div>
+          {
+            this.props.editMarks ?
+              <div className="input-group-append">
+                <button className={ this.state.ratingValue ? 'btn btn-success' : 'btn btn-danger' } type="button" onClick={this.updateRatingValue}>
+                  Change Rating
+                </button>
+              </div> :
+              <div className="input-group-append">
+                <button className="btn btn-info" type="button" onClick={this.updateGameValue}>
+                  Update
+                </button>
+                <button className="btn btn-danger" type="button" onClick={() => this.props.deleteGameValue(this.props.gameValue.gameId, this.props.gameValue.itemId)}>
+                  Delete
+                </button>
+              </div>
+          }
         </div>
       </div>
     )
